30d6eed5862974f5586a4b6b89ae43f85eed3bd2
Ankit Kapoor <Delta student>
2024-10-18 13:52:08 +0530

    Add Django to requirements

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#mxrh@!cl+(w72t^wv65n+)e8$ibbig71wfjv#6e)(gfjaj52u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'glide23.onrender.com','localhost','127.0.0.1',]
+
 
 
 # Application definition
